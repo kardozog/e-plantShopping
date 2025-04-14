0657a632b56a2d2dcc3c9d1dc93f1a5d55a7cb38
kardozog <kardemgunday@gmail.com>
2025-04-14 17:54:04 +0200

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -25,19 +25,19 @@ const CartItem = ({ onContinueShopping }) => {
 
 
   const handleIncrement = (item) => {
-    dispatch(updateQuantity(item));
-  };
+  dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+};
 
-  const handleDecrement = (item) => {
-   if (item && item.quantity> 1) {
-    dispatch(updateQuantity(item))
-   } else if (item.quantity === 0) {
-    dispatch(removeItem(item))
-   };
-  };
+const handleDecrement = (item) => {
+  if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item))
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
