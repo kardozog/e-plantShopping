c8056c95efe9f1516a51ecaf37fd5013f3003e07
kardem gunday <kardemgunday@gmail.com>
2025-04-14 09:34:38 -0400

    first

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,8 +274,18 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    {plantsArray.map ((category,index)=>(
+                        <div key={index} className="product-list"> 
+                        {category.plants.map((plant,plantIndex)=> (
+                            <div className="product-card" key={plantIndex}>
+                                <img className="product-image" src='{plant.image}' alt={plant.name} />
+                                <div className="product-title"> {plant.name} </div>
+                                <div className="product-title"> {plant.description} </div>
+                                <div className="product-price"> {plant.cost} </div> 
+                                <button className='product-button' onClick={()=> handleAddToCart(plant)}>Add to Cart</button>  
+                                </div>
+                        ))} </div>
+                    ))}
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
